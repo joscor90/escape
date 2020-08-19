6a904812c88f1251363028958ac369baaef235d1
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 14:16:40 -0400

    fixed incorrect vaue of an ARGV index and add a puts statement

--- escape.rb
+++ escape.rb
@@ -1,7 +1,7 @@
 g = ARGV[0].to_f   #Gravity variable given by the user.
-r = ARGV[0].to_f   #Planet radius given by the user.
+r = ARGV[1].to_f   #Planet radius given by the user.
 
-escape = (Math.sqrt(2*g*r)).truncate(2) 
+escape = Math.sqrt(2*g*r).truncate(2) #This expression allows to estimate the escape velocity of a given planet.
 
-
+puts "The escape velocity for this planet is: #{escape} m/s"
 
