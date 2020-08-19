becf26607b84350d73f09f75895124d5419e4098
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 14:12:04 -0400

    added planet's escape velocity formula

--- escape.rb
+++ escape.rb
@@ -1,3 +1,7 @@
-g = ARGV[0].to_f
-r = ARGV[0].to_f
+g = ARGV[0].to_f   #Gravity variable given by the user.
+r = ARGV[0].to_f   #Planet radius given by the user.
+
+escape = (Math.sqrt(2*g*r)).truncate(2) 
+
+
 
